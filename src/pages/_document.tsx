a1d35237bf27e4cbd0cf94d9072343612b694a6c
Gripers <gripers91@gmail.com>
2023-02-14 20:52:47 +0500

    third_cmt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,16 +9,15 @@ export default function Document() {
         <meta name='description' content='' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
-
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
         <Script
           src='https://code.jquery.com/jquery-3.6.3.min.js'
           integrity='sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU='
           crossOrigin='anonymous'
         ></Script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
       </body>
     </Html>
   );
